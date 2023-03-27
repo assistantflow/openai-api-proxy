c9d7d459c805edb8e8ef8e56fe11fc2106ba44a6
xinlingchao <xinlingchao@tal.com>
2023-03-27 13:01:33 +0800

    Use httpx

--- server/main.py
+++ server/main.py
@@ -1,46 +1,41 @@
-from urllib.parse import urlparse
-from starlette.background import BackgroundTask
-import aiohttp
+import httpx
 import uvicorn
 from fastapi import FastAPI, Request
-from sse_starlette.sse import EventSourceResponse
+from fastapi.responses import StreamingResponse
+from fastapi.background import BackgroundTasks
 
 
 app = FastAPI()
-aiohttpClient: aiohttp.ClientSession = None
 
 
 @app.on_event("startup")
 async def startup_event():
-    global aiohttpClient
-    aiohttpClient = aiohttp.ClientSession(base_url="https://api.openai.com", timeout=aiohttp.ClientTimeout(total=30))
+    global httpxClient
+    httpxClient = httpx.AsyncClient(base_url="https://api.openai.com", timeout=60)
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    await aiohttpClient.close()
+    await httpxClient.aclose()
 
 
 @app.api_route("/v1/completions", methods=["GET", "POST", "OPTIONS"])
 @app.api_route("/v1/chat/completions", methods=["GET", "POST", "OPTIONS"])
-async def proxy(req: Request) -> EventSourceResponse:
-    _headers = req.headers.mutablecopy()
-    _headers["host"] = aiohttpClient._base_url.host
-    req._headers = _headers
+async def proxy(req: Request):
+    nh = req.headers.mutablecopy()
+    nh["host"] = httpxClient._base_url.host
+    req._headers = nh
     req.scope.update(headers=req.headers.raw)
 
-    if req.method == "OPTIONS":
-        body = None
-    else:
-        body = await req.json()
-    resp = await aiohttpClient.request(req.method, req.url.path, headers=req.headers, json=body)
+    url = httpx.URL(path=req.url.path, query=req.url.query.encode("utf-8"))
+    br = httpxClient.build_request(req.method, url, headers=req.headers.raw, content=await req.body())
+    resp = await httpxClient.send(br, stream=True)
 
-    async def event_generator():
-        while line := await resp.content.readline():
-            yield line
-
-    return EventSourceResponse(
-        event_generator(), status_code=resp.status, headers=resp.headers, background=BackgroundTask(resp.close)
+    return StreamingResponse(
+        resp.aiter_raw(),
+        status_code=resp.status_code,
+        headers=resp.headers,
+        background=BackgroundTasks([resp.aclose])
     )
 
 
