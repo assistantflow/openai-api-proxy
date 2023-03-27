2a7f3b9c8b338b06b7ba7e5abc0219d90c7bf57b
xinlingchao <xinlingchao@tal.com>
2023-03-27 13:57:34 +0800

    Prepare body filter and command-line deploy

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import click
 import httpx
 import uvicorn
 from fastapi import FastAPI, Request
@@ -6,6 +7,7 @@ from fastapi.background import BackgroundTasks
 
 
 app = FastAPI()
+httpxClient: httpx.AsyncClient = None
 
 
 @app.on_event("startup")
@@ -21,7 +23,7 @@ async def shutdown_event():
 
 @app.api_route("/v1/completions", methods=["GET", "POST", "OPTIONS"])
 @app.api_route("/v1/chat/completions", methods=["GET", "POST", "OPTIONS"])
-async def proxy(req: Request):
+async def proxy(req: Request) -> StreamingResponse:
     nh = req.headers.mutablecopy()
     nh["host"] = httpxClient._base_url.host
     req._headers = nh
@@ -31,13 +33,17 @@ async def proxy(req: Request):
     br = httpxClient.build_request(req.method, url, headers=req.headers.raw, content=await req.body())
     resp = await httpxClient.send(br, stream=True)
 
+    async def echo():
+        async for line in resp.aiter_lines():
+            yield line
+
     return StreamingResponse(
-        resp.aiter_raw(),
-        status_code=resp.status_code,
-        headers=resp.headers,
-        background=BackgroundTasks([resp.aclose])
+        echo(), status_code=resp.status_code, headers=resp.headers, background=BackgroundTasks([resp.aclose])
     )
 
 
-def start():
-    uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
+@click.command
+@click.option("--port", default=8000, help="the server port", show_default=True)
+@click.option("--reload", default=False, help="enable reload mode for development", show_default=True)
+def start(port: int, reload: bool):
+    uvicorn.run("server.main:app", host="0.0.0.0", port=port, reload=reload)
